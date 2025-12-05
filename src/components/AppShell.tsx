39357a811f1527720bf46350eae91fbe9ec39741
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-12-05 22:34:05 +0530

    feat: enhance AppShell to conditionally render based on authentication pages for side bar

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -4,16 +4,29 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
 import Sidebar from "./Sidebar";
+import { usePathname } from "next/navigation"; 
 
 export default function AppShell({ children }: { children: React.ReactNode }) {
-
   const { sidebarOpen } = useSelector((state: RootState) => state.ui);
+  const pathname = usePathname(); 
+
+  const isAuthPage = ["/login", "/signup"].includes(pathname);
+
+  if (isAuthPage) {
+    return (
+      <main className="w-full min-h-screen">
+        {children}
+      </main>
+    );
+  }
 
   return (
     <div className="flex pt-16">
+
       <div className={`${sidebarOpen ? "block" : "hidden md:hidden"} `}>
          <Sidebar />
       </div>
+    
       <main className={`w-full min-h-screen transition-all duration-300 ${sidebarOpen ? "md:pl-64" : "pl-0"}`}>
         {children}
       </main>
